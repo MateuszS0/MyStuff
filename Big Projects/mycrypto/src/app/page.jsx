dcee0db17a9b51836fb419f3222fb4000f7fab36
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-10-04 16:47:36 +0200

    feat: file fixes

---
+++ Big Projects/mycrypto/src/app/page.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <section className="w-full flex flex-col items-center">
+        <h2 className="justify-center text-2xl">Welcome to</h2>
+        <h1 className="justify-center text-6xl text_gradient">CryptoVault</h1>
+    </section>
+  )
+}
+
+export default Home
