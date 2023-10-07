ae64237c290fd444f90aef006766a3d5c19e9586
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-10-07 15:51:18 +0200

    feat: original price console.log

--- Big Projects/store/lib/scraper/index.ts
+++ Big Projects/store/lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
@@ -27,8 +28,23 @@ export async function scrapeAmazonProduct(url: string) {
         const $ = cheerio.load(response.data);
         
         const title = $('#productTitle').text().trim();
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+            $('.a-price.a-text-price'),
+        );
 
-        console.log(title);
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('span.a-offscreen a-price.a-text-price'),
+            $('span.a-price a-text-price'),
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('#corePriceDisplay_mobile_feature_div'),
+            $('a-size-base.a-color-price'),
+        );
+        console.log("title: " +title, "\ncurrent Price: "+currentPrice, "\noriginal Price: "+originalPrice);
 
     } catch (error: any) {
         throw new Error(`Failed to scrape product: ${error.message}`)
