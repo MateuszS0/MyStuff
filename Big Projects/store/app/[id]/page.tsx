10e259a57ad67203f7d0717f5c4d75eb3bade3e4
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-10-24 14:37:37 +0200

    feat: product description

--- Big Projects/store/app/[id]/page.tsx
+++ Big Projects/store/app/[id]/page.tsx
@@ -14,7 +14,7 @@ type Props = {
 const ProductDetails = async ({ params: { id } }: Props) => {
   const product = await getProductById(id);
 
-  if (!product) redirect('/');
+  // if (!product) redirect('/');
 
   return (
     <div className='product-container'>
@@ -145,7 +145,28 @@ const ProductDetails = async ({ params: { id } }: Props) => {
 
         </div>
       </div>
+      <div className="flex flex-col gap-16">
+        <div className="flex flex-col gap-5">
+          <h3 className="text-2xl text-secondary font-semibold">
+            Product Description
+          </h3>
+          <div className="flex flex-col gap-4">
+            {product?.description?.split('\n')}
+          </div>
+        </div>
+        <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
+          <Image 
+            src="/assets/icons/bag.svg"
+            alt="check"
+            width={22}
+            height={22}
+          />
 
+          <Link href="/" className="text-base text-white">
+            Buy Now
+          </Link>
+        </button>
+      </div>
 
     </div>
   )
